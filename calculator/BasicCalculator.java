268373ba8c8a425b8679668268ca53990146b156
Tyson <tysonmcleod@gmail.com>
2021-02-05 10:05:14 +0100

    test

--- calculator/BasicCalculator.java
+++ calculator/BasicCalculator.java
@@ -1,6 +1,51 @@
+import java.util.Scanner;
+
+
+
 class BasicCalculator{
   public static void main(String args[]){
-    System.out.println("Hello world");
-    
+
+
+      Scanner input = new Scanner(System.in);
+      Scanner inputOp = new Scanner(System.in);
+      double first, second, answer;
+      String operation;
+
+      System.out.println("Enter the first number: ");
+      first = input.nextDouble();
+
+      System.out.println("Enter the operation: ");
+      operation = inputOp.nextLine();
+
+      System.out.println("Enter the second number: ");
+      second = input.nextDouble();
+
+
+      switch (operation) {
+        case "+":
+          answer = first + second;
+          System.out.println("The answer: " + answer);
+          break;
+        case "-":
+          answer = first - second;
+          System.out.println("The answer: " + answer);
+          break;
+        case "%":
+          answer = first % second;
+          System.out.println("The answer: " + answer);
+          break;
+        case "*":
+          answer = first * second;
+          System.out.println("The answer: " + answer);
+          break;
+        case "x":
+          answer = first * second;
+          System.out.println("The answer: " + answer);
+          break;
+        default:
+          System.out.println("you broke it");
+
+      }
+
   }
 }
